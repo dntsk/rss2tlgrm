1b608af72c26c0d107404f39ec5389ddfe1747d0
Dmitriy Lyalyuev <dmitriy@lyalyuev.info>
2020-08-03 16:50:52 +0300

    fix: sleep timer

--- rss2tlgrm/cron.py
+++ rss2tlgrm/cron.py
@@ -18,15 +18,19 @@ class FetchRSS(CronJobBase):
         bot = telebot.TeleBot(os.getenv("TGBOT_TOKEN"))
         feeds = Feed.objects.all()
         for f in feeds:
-            d = feedparser.parse(f.feed)
-            for i in d.entries:
-                if i.link != f.last_item:
-                    link = i.link.split('?')[0]
-                    print(f'{f.channel}: {i.title} {link}')
-                    message = f"{i.title}\n\n{link}"
-                    bot.send_message(f'@{f.channel}', message)
-                    time.sleep(5)
-                else:
-                    break
-            f.last_item = d.entries[0].link
-            f.save()
+            if f.active:
+                print(f'Processing {f.name}.')
+                d = feedparser.parse(f.feed)
+                for i in d.entries[:20]:
+                    if i.link != f.last_item:
+                        link = i.link.split('?')[0]
+                        print(f'{f.channel}: {i.title} {link}')
+                        message = f"{i.title}\n\n{link}"
+                        bot.send_message(f'@{f.channel}', message)
+                        time.sleep(10)
+                    else:
+                        break
+                f.last_item = d.entries[0].link
+                f.save()
+            else:
+                print(f'Skipping "{f.name}" due to not active.')
