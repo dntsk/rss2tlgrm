f02946966c3565e8a3c75e310d7accd299a0a720
Dmitriy Lyalyuev <dmitriy@lyalyuev.info>
2020-08-03 17:21:50 +0300

    feat: show another fields on admin page

--- rss2tlgrm/admin.py
+++ rss2tlgrm/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Feed
 
 
-admin.site.register(Feed)
+class FeedAdmin(admin.ModelAdmin):
+    list_display = ('name', 'feed', 'channel', 'active')
+
+admin.site.register(Feed, FeedAdmin)
