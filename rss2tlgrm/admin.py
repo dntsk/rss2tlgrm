5eb7e356973d6123c13d46ebba54998b89f16a0d
Dmitriy Lyalyuev <dmitriy@lyalyuev.info>
2020-08-04 09:35:01 +0300

    feature: Posts in Admin

--- rss2tlgrm/admin.py
+++ rss2tlgrm/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Feed
+from .models import Feed, Post
 
 
 class FeedAdmin(admin.ModelAdmin):
     list_display = ('name', 'feed', 'channel', 'active')
 
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('url', 'date')
+
+
 admin.site.register(Feed, FeedAdmin)
+admin.site.register(Post, PostAdmin)
