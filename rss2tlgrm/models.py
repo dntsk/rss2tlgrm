32ec3fe8e5f16753ce3af199eb9d5abdf08a514b
Dmitriy Lyalyuev <dmitriy@lyalyuev.info>
2020-08-04 09:33:00 +0300

    remove: last item from Feed model

--- rss2tlgrm/models.py
+++ rss2tlgrm/models.py
@@ -6,7 +6,6 @@ class Feed(models.Model):
     name = models.CharField(max_length=300)
     feed = models.URLField()
     active = models.BooleanField(default=True)
-    last_item = models.URLField(blank=True, null=True)
     channel = models.CharField(max_length=100)
 
     def __str__(self):
