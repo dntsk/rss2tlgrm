82c4aecf4883475076252057d651c8f7bbb46d45
Dmitriy Lyalyuev <dmitriy@lyalyuev.info>
2022-02-10 15:09:12 +0200

    remove sentry

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.7-alpine
+FROM python:3.10-alpine
 
 ADD requirements.txt /opt/requirements.txt
 ADD fetchfeeds /etc/periodic/15min/fetchfeeds
--- rss2tlgrm/settings.py
+++ rss2tlgrm/settings.py
@@ -11,22 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-
-sentry_sdk.init(
-    dsn="https://ad692a9150cc4436b2c23a4c9f005f35@o823691.ingest.sentry.io/5812666",
-    integrations=[DjangoIntegration()],
-
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
-
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
