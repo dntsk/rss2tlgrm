8e5c39a14f64136a65f1a91eff1c4501eaef898b
Dmitriy Lyalyuev <dmitriy@lyalyuev.info>
2022-06-15 16:28:17 +0200

    build tags and branches

--- rss2tlgrm/settings.py
+++ rss2tlgrm/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kj%u+n=akh7fbxqh)6mc0s_w#^2ee3a995z-2-7ram4ew^8h(n'
+SECRET_KEY = os.getenv('SECRET_KEY', 'kj%u+n=akh7fbxqh)6mc0s_w#^2ee3a995z-2-7ram4ew^8h(n')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
